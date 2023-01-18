a2355a93f0d07fa1b169469ba028f8c2ad9cee90
EC2 Default User <ec2-user@ip-172-31-33-125.ap-northeast-1.compute.internal>
2023-01-18 06:18:56 +0000

    会員サインアップ機能導入

--- db/migrate/20230117040747_devise_create_customers.rb
+++ db/migrate/20230117040747_devise_create_customers.rb
@@ -6,6 +6,16 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :address, null: false
+      t.string :phone_number, null: false
+      t.string :address_number, null: false
+      t.boolean :is_end, null: false, default: false
+
+
 
       ## Recoverable
       t.string   :reset_password_token
