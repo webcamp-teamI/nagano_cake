0d52040b05b4e46e2fcf0d39a9c6e81eb6d455fc
EC2 Default User <ec2-user@ip-172-31-33-125.ap-northeast-1.compute.internal>
2023-01-19 08:21:32 +0000

    ルートとURLの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,61 @@
 Rails.application.routes.draw do
+  # get 'items' => 'items#show'
 
-
-  namespace :public do
-    get 'deliveries/index'
-    get 'deliveries/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/check'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/confirm'
-  end
-  namespace :public do
-    get 'carts/index'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-    get 'home/about' => "homes#about", as: "about"
+  # 会員側のルーティング設定
+  scope module: :public do
+    get 'about' => "homes#about", as: "about"
     root to: "homes#top"
+    resources :items, only: [:show, :index]
+    resource :customers, only: [:edit, :update, :destroy]
+    get 'customers/confirm' => 'customers#confirm'
+    get 'customers' => 'customers#show'
+    resources :carts, only: [:index, :update, :destroy, :create]
+    delete 'carts/destroy_all' => 'carts#destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/check' => 'orders#check'
+    get 'orders/complete' => 'orders#complete'
+    resources :deliveries, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  # 管理者側のルーティング設定
+  namespace :admin do
+    get '/' => "homes#top"
+    resources :items, only: [:new, :create, :show, :index, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index,:edit, :update, :show]
+    resources :orders, only: [:show, :update]
+    resources :orders_details, only: [:update]
+    # get 'items' => 'admin/items#index'
+    # get 'items' => 'admin/items#show'
+  end
+
+
+
+  # namespace :public do
+  #   get 'deliveries/index'
+  #   get 'deliveries/edit'
+  # end
+  # namespace :public do
+  #   get 'orders/new'
+  #   get 'orders/check'
+  #   get 'orders/complete'
+  #   get 'orders/index'
+  #   get 'orders/show'
+  # end
+  # namespace :public do
+  #   get 'customers/show'
+  #   get 'customers/edit'
+  #   get 'customers/confirm'
+  # end
+  # namespace :public do
+  #   get 'carts/index'
+  # end
+  # namespace :public do
+  #   get 'homes/top'
+  #   get 'homes/about'
+  # end
+
+
 
   # 顧客用
   # URL /customers/sign_in ...
