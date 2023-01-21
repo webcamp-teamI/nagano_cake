f664e07ce94ca3ab3bebce572aa206a69784c567
EC2 Default User <ec2-user@ip-172-31-33-125.ap-northeast-1.compute.internal>
2023-01-21 04:29:15 +0000

    admin/item new.showの機能のみ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,12 +3,17 @@ def index
 end
 
 def new
+  @item = Item.new
 end
 
 def create
+  @item = Item.new(item_params)
+  @item.save
+  redirect_to admin_items_path
 end
 
 def show
+  @item = Item.find(params[:id])
 end
 
 def edit
@@ -16,4 +21,9 @@ end
 
 def update
 end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :genre_id, :name, :detail, :tax_free, :is_active)
+  end
 end
