055c09da39e9f49e3bc7d24ead51dde1956e5251
tokudatakato <119557028+tokudatakato@users.noreply.github.com>
2023-01-21 13:53:35 +0900

    Revert " Items/admin new.showの機能のみ作成しました。"

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,17 +3,12 @@ def index
 end
 
 def new
-  @item = Item.new
 end
 
 def create
-  @item = Item.new(item_params)
-  @item.save
-  redirect_to admin_items_path
 end
 
 def show
-  @item = Item.find(params[:id])
 end
 
 def edit
@@ -21,9 +16,4 @@ end
 
 def update
 end
-
-  private
-  def item_params
-    params.require(:item).permit(:image, :genre_id, :name, :detail, :tax_free, :is_active)
-  end
 end
