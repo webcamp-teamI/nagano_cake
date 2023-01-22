d790c7c5440faecfa696dc1ed49d241f36c130dd
EC2 Default User <ec2-user@ip-172-31-42-90.ap-northeast-1.compute.internal>
2023-01-22 06:38:48 +0000

    admin_customer_view

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,23 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all.page(params[:page])
   end
     
   def show
+    @customer = Customer.find(params[:id])
   end
     
-  def edit 
+  def edit
+    @customer = Customer.find(params[:id])
   end
     
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(@customer)
+     flash[:notice] = "You have updated book successfully."
+     redirect_to admin_customers_path
+    else
+     render:edit
+    end
   end
 end
