9476376577b2b5c0a41ccec431352900e252384e
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2023-01-24 09:33:11 +0000

    注文機能作成中

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(email: 'test@admin', password: 'teamII')
+#Admin.create!(email: 'test@admin', password: 'teamII')
 
-Genre.create!(id: 1, genre_name: 'ケーキ')
-Genre.create!(id: 2, genre_name: 'プリン')
-Genre.create!(id: 3, genre_name: '焼き菓子')
+#Genre.create!(id: 1, genre_name: 'ケーキ')
+#Genre.create!(id: 2, genre_name: 'プリン')
+#Genre.create!(id: 3, genre_name: '焼き菓子')
 
-Item.create!(genre_id: 1, name: 'ケーキ', detail: 'ケーキです', tax_free: 3500)
-Item.create!(genre_id: 2, name: 'プリン', detail: 'プリンです', tax_free: 400)
-Item.create!(genre_id: 3, name: '焼き菓子', detail: '焼き菓子です', tax_free: 3500)
+#Item.create!(genre_id: 1, name: 'ケーキ', detail: 'ケーキです', tax_free: 3500)
+#Item.create!(genre_id: 2, name: 'プリン', detail: 'プリンです', tax_free: 400)
+#Item.create!(genre_id: 3, name: '焼き菓子', detail: '焼き菓子です', tax_free: 3500)
+
+Cart.create!(customer_id: 3, item_id: 1, count: 2)
