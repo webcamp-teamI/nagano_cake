371c505105358118e47241b252af09fc7b421799
EC2 Default User <ec2-user@ip-172-31-33-125.ap-northeast-1.compute.internal>
2023-01-27 04:21:01 +0000

    注文詳細機能作成中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,15 @@
 class Admin::OrderDetailsController < ApplicationController
-    def show
-    end
-    
+
     def update
+
+       @order_detail = OrderDetail.find(params[:id])
+       @order = @order_detail.order
+       @order_detail.update(order_detail_params)
+       redirect_to admin_order_path(@order.id)
+    end
+    private
+
+    def order_detail_params
+      params.require(:order_detail).permit(:make_status)
     end
 end
