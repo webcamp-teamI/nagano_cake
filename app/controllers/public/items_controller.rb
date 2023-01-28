6ac2262b3925ca1d47e76ea91467c72903542729
EC2 Default User <ec2-user@ip-172-31-42-90.ap-northeast-1.compute.internal>
2023-01-28 08:52:13 +0000

    レイアウトを修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,7 +2,7 @@ class Public::ItemsController < ApplicationController
   def index
     @item = Item.new
     # @items = Item.all
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).per(8)
   end
 
   def show
